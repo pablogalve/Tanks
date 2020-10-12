7bdecb3a00f1e3a716d70537fc652d0279f4f415
Pablo <eduinvestor100@gmail.com>
2020-10-12 17:40:43 +0200

    Wander AI movement implemented

--- Tank Tutorial/Assets/Scripts/Tank/Patrol.cs
+++ Tank Tutorial/Assets/Scripts/Tank/Patrol.cs
@@ -1,45 +1,42 @@
 ﻿using UnityEngine;
-    using UnityEngine.AI;
-    using System.Collections;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
 
 
-    public class Patrol : MonoBehaviour {
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
 
-        public Transform[] points;
-        private int destPoint = 0;
-        private NavMeshAgent agent;
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
 
-
-        void Start () {
-            agent = GetComponent<NavMeshAgent>();
-
-            // Disabling auto-braking allows for continuous movement
-            // between points (ie, the agent doesn't slow down as it
-            // approaches a destination point).
-            agent.autoBraking = false;
-
-            GotoNextPoint();
-        }
-
-
-        void GotoNextPoint() {
-            // Returns if no points have been set up
-            if (points.Length == 0)
-                return;
-
-            // Set the agent to go to the currently selected destination.
-            agent.destination = points[destPoint].position;
-
-            // Choose the next point in the array as the destination,
-            // cycling to the start if necessary.
-            destPoint = (destPoint + 1) % points.Length;
-        }
-
-
-        void Update () {
-            // Choose the next destination point when the agent gets
-            // close to the current one.
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                GotoNextPoint();
-        }
+        GotoNextPoint();
     }
+
+    void Update () {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void GotoNextPoint() {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+}
---
+++ Tank Tutorial/Assets/Scripts/Tank/WanderMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 1.0f)
+        {
+            Vector3 current_position = transform.position; //Get current position
+            agent.destination = Wander(current_position, 50.0f, 1);
+        }
+            
+    }
+
+    public static Vector3 Wander (Vector3 origin, float distance, int layermask) {
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+            randomDirection += origin;
+           
+            NavMeshHit navHit;
+           
+            NavMesh.SamplePosition (randomDirection, out navHit, distance, layermask);
+           
+            return navHit.position;
+        }
+}
