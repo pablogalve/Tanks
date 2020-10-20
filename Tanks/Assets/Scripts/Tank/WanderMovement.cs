9af4ca0db7da48e7c14cb0f59d2c473a213b851c
Pablo <eduinvestor100@gmail.com>
2020-10-20 14:48:18 +0200

    Wander() bug fixed, now the tank doesn't get stuck

---
+++ Tanks/Assets/Scripts/Tank/WanderMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Vector3 previousPosition;
+    public float currentSpeed;
+    public float targetSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        targetSpeed = GetComponent<NavMeshAgent>().speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            SetNewDestination(50.0f);
+        }
+
+        //Calculate current speed
+        Vector3 curMove = transform.position - previousPosition;
+        currentSpeed = curMove.magnitude / Time.deltaTime;
+        previousPosition = transform.position;
+
+        //If tanks gets stucked, set another destination
+        if(currentSpeed <= targetSpeed * 0.1f)
+        {
+            SetNewDestination(200.0f);
+        }
+    }
+
+    public void SetNewDestination(float distance)
+    {
+        Vector3 current_position = transform.position; //Get current position
+        agent.destination = Wander(current_position, distance, 1);
+    }
+
+    public static Vector3 Wander (Vector3 origin, float distance, int layermask) {
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+            randomDirection += origin;
+           
+            NavMeshHit navHit;
+           
+            NavMesh.SamplePosition (randomDirection, out navHit, distance, layermask);
+           
+            return navHit.position;
+        }
+}
